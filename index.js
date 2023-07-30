981634108d37215af2a7880678eb32284db70403
Vitor Moutim <moutimg@gmail.com>
2023-07-30 14:21:30 -0300

    feat: adiciona cors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 
+const cors = require('cors');
+
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
+  app.use(cors());
+  next();
+});
 
 const routes = require('./src/routes');
 
